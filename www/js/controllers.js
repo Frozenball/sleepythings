68e672a5485f553f81fee38e49ff23c64bc72ad2
Teemu Kokkonen <teemu.on.ihminen@gmail.com>
2016-04-20 14:58:40 +0300

    Initial commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,27 @@
 angular.module('app.controllers', [])
-  
+
 .controller('alarmCtrl', function($scope) {
-
 })
-   
+
 .controller('statisticsCtrl', function($scope) {
-
 })
-   
+
 .controller('smartThermostatCtrl', function($scope) {
-
+  $scope.night = 18;
+  $scope.day = 22;
+  $scope.active = true;
 })
-   
+
 .controller('smartLightsCtrl', function($scope) {
-
+  $scope.active = true;
 })
-   
+
 .controller('tVCtrl', function($scope) {
-
+  $scope.active = true;
 })
-      
+
 .controller('pageCtrl', function($scope) {
-
 })
-   
+
 .controller('devicesCtrl', function($scope) {
-
 })
- 
