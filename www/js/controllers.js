88729395188cb6d7496a75c419c06226b60caf98
Teemu Kokkonen <teemu.on.ihminen@gmail.com>
2016-04-21 11:36:02 +0300

    Stats etc.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,16 @@ angular.module('app.controllers', [])
 .controller('alarmCtrl', function($scope) {
 })
 
-.controller('statisticsCtrl', function($scope) {
+.controller('statisticsCtrl', function($scope, $timeout) {
+
+    $scope.labels = ['17.4', '18.4', '19.4', '20.4', '21.4', '22.4'];
+    $scope.series = ['Hours slept'];
+    $scope.data = [
+      [7.6, 6.7, 7.4, 7.1, 7.5, 8.1]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
 })
 
 .controller('smartThermostatCtrl', function($scope) {
